56e9a933ddd53482a901b86516a6215ff3d75230
Dharmik Patel <ddp10048971@gmail.com>
2022-08-04 12:31:48 -0400

    dashboard

--- Frontend/better-pay/src/components/Dashboard.js
+++ Frontend/better-pay/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Header from './Header'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Header></Header>
+      <p>dashboard</p>
+    </div>
   )
 }
 
