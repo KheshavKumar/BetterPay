d603519246c5ef0917db0a4f3df2067256c85a88
Dharmik Patel <ddp10048971@gmail.com>
2022-08-04 18:00:16 -0400

    finished middle bottom half

--- Frontend/better-pay/src/components/Home.js
+++ Frontend/better-pay/src/components/Home.js
@@ -2,18 +2,36 @@ import React from "react";
 import Header from "./Header";
 import HomeTopHalf from "./HomeTopHalf";
 import HomeMiddleHalfTop from "./HomeMiddleHalfTop";
+import HomeMiddleHalfBottom from "./HomeMiddleHalfBottom";
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <br></br>
       <HomeTopHalf></HomeTopHalf>
+      <div>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
+      </div>
       <HomeMiddleHalfTop></HomeMiddleHalfTop>
+      <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      </div>
+      <HomeMiddleHalfBottom></HomeMiddleHalfBottom>
+      <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      </div>
     </div>
   );
 };
