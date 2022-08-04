c0749776039aeb46b0d4112ee8eaaf20b9458e8d
Dharmik Patel <ddp10048971@gmail.com>
2022-08-03 22:53:39 -0400

    basic compoents added

---
+++ Frontend/better-pay/src/componets/person.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const person = () => {
+  return (
+    <div>person</div>
+  )
+}
+/*
+- unique id
+- name
+- min intreset rate
+- max loan amoint
+- raiting
+= fake credit score
+= fake usernam and pass
+- [list of thier freinds]
+- [list of loans taken out]
+- [list of loans given out]
+*/
+export default person
