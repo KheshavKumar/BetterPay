2eadf79083eb31c3634a944419a881f5644ccc76
Manjot Kaur <manjotkaur@Manjots-MacBook-Pro.local>
2022-08-03 23:08:29 -0400

    original socket

---
+++ Backend/server.py
@@ -0,0 +1,21 @@
+import socket
+LOCALHOST = "192.168.1.134"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((LOCALHOST, PORT))
+server.listen(1)
+print("Server started")
+print("Waiting for client request..")
+clientConnection,clientAddress = server.accept()
+print("Connected clinet :" , clientAddress)
+msg = ''
+while True:
+  in_data = clientConnection.recv(1024)
+  msg = in_data.decode()
+  if msg=='bye':
+    break
+  print("From Client :" , msg)
+  out_data = input()
+  clientConnection.send(bytes(out_data,'UTF-8'))
+print("Client disconnected....")
+clientConnection.close()
