d2f4e613db1e2e86c69a83ffa0a6c177ad0b1876
KheshavKumar <kheshavkumar@gmail.com>
2022-08-04 12:04:08 -0400

    Home re-added

---
+++ Frontend/better-pay/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      Home Page
+    </div>
+  );
+};
+
+export default Home;
