fafe3a03e52008af801866f728afb7910c49ad2c
Dharmik Patel <ddp10048971@gmail.com>
2022-08-05 11:10:34 -0400

    added dog

--- Frontend/better-pay/src/components/HomeMiddleHalfBottom.js
+++ Frontend/better-pay/src/components/HomeMiddleHalfBottom.js
@@ -9,6 +9,8 @@ import customerImage1 from '../assets/customer1.png';
 import customerImage2 from '../assets/customer2.png';
 import customerImage3 from '../assets/customer3.png';
 import customerImage4 from '../assets/customer4.png';
+import dogImage from '../assets/dogImage.png';
+
 import darkGradient from '../assets/Shape.png';
 
 
@@ -16,7 +18,7 @@ import './HomeMiddleHalfBottom.css'
 const HomeMiddleHalfBottom = () => {
     return (
         <div>
-            
+
             <Container>
                 <Row className="justify-content-md-center">
                     <Col>
@@ -38,13 +40,31 @@ const HomeMiddleHalfBottom = () => {
                                         <Card.Text className='custImageOverlayHolderSubTilte'>User from 2022</Card.Text>
                                     </Card.ImgOverlay>
                                 </Card>
-                                <Card border="light" className="text-white" style={{ width: '15rem' }}>
-                                    <Card.Img src={customerImage3} alt="Card image" />
-                                    <Card.ImgOverlay className='custImageOverlayHolder'>
-                                        <Card.Title className='custImageOverlayHolderTitle' >Sandra Hughes</Card.Title>
-                                        <Card.Text className='custImageOverlayHolderSubTilte'>User from 2022</Card.Text>
-                                    </Card.ImgOverlay>
-                                </Card>
+                                <div class="avatar">
+                                    <Card border="light" className="text-white avatarImage" style={{ width: '15rem' }}>
+                                        <Card.Img src={customerImage3} className="" alt="Card image">
+                                        </Card.Img>
+                                        <Card.ImgOverlay className='custImageOverlayHolder'>
+                                            <Card.Title className='custImageOverlayHolderTitle' >Sandra Hughes</Card.Title>
+                                            <Card.Text className='custImageOverlayHolderSubTilte'>User from 2022</Card.Text>
+                                        </Card.ImgOverlay>
+                                    </Card>
+
+                                    <div class="overlay">
+                                    <Card border="light" className="text-white avatarImage" style={{ width: '15rem' }}>
+                                        <Card.Img src={dogImage} className="" alt="Card image">
+                                        </Card.Img>
+                                        <Card.ImgOverlay className='custImageOverlayHolder'>
+                                            <Card.Title className='custImageOverlayHolderTitle' >Cooper</Card.Title>
+                                            <Card.Text className='custImageOverlayHolderSubTilte'>User from 2022</Card.Text>
+                                        </Card.ImgOverlay>
+                                    </Card>                                    </div>
+                                </div>
+
+
+
+
+
                                 <Card border="light" className="text-white" style={{ width: '15rem' }}>
                                     <Card.Img src={customerImage4} alt="Card image" />
                                     <Card.ImgOverlay className='custImageOverlayHolder'>
